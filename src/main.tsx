b92fd90d60b1a15d4a185c6bc7186ffe83c83937
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-19 20:22:06 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react"; // Добавлено React
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
