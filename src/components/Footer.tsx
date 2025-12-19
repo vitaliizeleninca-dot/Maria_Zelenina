8188fc9af99686e51cdd5a0e0e65a930d869920e
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-19 19:53:38 +0200

    Add Footer component with copyright notice

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-lavender text-white/60 py-8 text-center">
+      <div className="container mx-auto px-6">
+        <p className="font-serif">
+          &copy; {new Date().getFullYear()} Maria Magdalena Zelenina. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
