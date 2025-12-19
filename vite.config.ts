3d597db3b31ac96f75d4ec877bd69670794f8cc2
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-19 21:38:53 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: "classic",
+      jsxRuntime: "automatic", // меняем на автоматический
     }),
   ],
 });
