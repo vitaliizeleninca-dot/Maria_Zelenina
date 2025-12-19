fd1057c01c85949abd101e232545cc4513aa2eb6
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-20 00:08:54 +0200

    Update PhotoGallery.tsx

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -6,7 +6,7 @@ import img4 from '../assets/4.jpg';
 import img5 from '../assets/7.jpg';
 
 // Массив в нужном порядке
-const images = [img1, img2, img3, img4, img5, img6];
+const images = [img1, img2, img3, img4, img5];
 
 const PhotoGallery = () => {
   return (
