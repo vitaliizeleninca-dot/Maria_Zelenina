4f3ae6a292062aa603f518c262d7888cd685b8b1
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-19 22:40:28 +0200

    Update PhotoGallery.tsx

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import moment1 from '../assets/1.jpg';
-import moment2 from '../assets/2.jpg';
-import moment3 from '../assets/3.jpg';
-import moment4 from '../assets/4.jpg';
-import moment5 from '../assets/6.jpg';
-import moment6 from '../assets/7.jpg';
+import moment1 from '../assets/7.jpg';
+import moment2 from '../assets/6.jpg';
+import moment3 from '../assets/4.jpg';
+import moment4 from '../assets/3.jpg';
+import moment5 from '../assets/2.jpg';
+import moment6 from '../assets/1.jpg';
 
 // Массив в том порядке, в котором хочешь показывать
 const images = [moment3, moment1, moment5, moment2, moment6, moment4];
