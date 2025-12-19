7b0ff736d528272eba5c8c975a0725308c9bf8dd
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-19 23:56:26 +0200

    Update PhotoGallery.tsx

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import img1 from '../assets/1.jpg';
 import img2 from '../assets/2.jpg';
-import img3 from '../assets/3.jpg';
+import img3 from '../assets/6.jpg';
 import img4 from '../assets/4.jpg';
-import img5 from '../assets/6.jpg';
+import img5 from '../assets/3.jpg';
 import img6 from '../assets/7.jpg';
 
 // Массив в нужном порядке
