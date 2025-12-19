2ca57dba47cf874ed0c96109a21834b73b0b83ea
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-19 22:24:09 +0200

    Update PhotoGallery.tsx

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import moment1 from '../assets/moment1.jpg';
+import moment2 from '../assets/moment2.jpg';
+import moment3 from '../assets/moment3.jpg';
+import moment4 from '../assets/moment4.jpg';
+import moment5 from '../assets/moment5.jpg';
+import moment6 from '../assets/moment6.jpg';
 
 const PhotoGallery = () => {
-  const images = [
-    "https://images.unsplash.com/photo-1759694430835-ef9350656e7d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NTAxMTZ8MHwxfHNlYXJjaHwxfHxyaHl0aG1pYyUyMGd5bW5hc3RpY3MlMjBob29wJTIwZ2lybHxlbnwwfHx8fDE3NjYxNjI3MTB8MA&ixlib=rb-4.1.0&q=80&w=1080",
-    "https://images.unsplash.com/photo-1611032079840-424c81ac82b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NTAxMTZ8MHwxfHNlYXJjaHwxfHxyaHl0aG1pYyUyMGd5bW5hc3RpY3MlMjByaWJib24lMjBnaXJsfGVufDB8fHx8MTc2NjE2MjcxMHww&ixlib=rb-4.1.0&q=80&w=1080",
-    "https://images.unsplash.com/photo-1747720663196-b75e5babb2de?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NTAxMTZ8MHwxfHNlYXJjaHw3fHxyaHl0aG1pYyUyMGd5bW5hc3RpY3MlMjBob29wJTIwZ2lybHxlbnwwfHx8fDE3NjYxNjI3MTB8MA&ixlib=rb-4.1.0&q=80&w=1080",
-    "https://images.unsplash.com/photo-1639998733481-b2cb5da248f5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NTAxMTZ8MHwxfHNlYXJjaHw1fHxyaHl0aG1pYyUyMGd5bW5hc3RpY3MlMjBob29wJTIwZ2lybHxlbnwwfHx8fDE3NjYxNjI3MTB8MA&ixlib=rb-4.1.0&q=80&w=1080",
-    "https://images.unsplash.com/photo-1764622078369-ca3930f0267b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NTAxMTZ8MHwxfHNlYXJjaHw5fHxyaHl0aG1pYyUyMGd5bW5hc3RpY3MlMjBob29wJTIwZ2lybHxlbnwwfHx8fDE3NjYxNjI3MTB8MA&ixlib=rb-4.1.0&q=80&w=1080",
-    "https://images.unsplash.com/photo-1598981553323-6d8dfe02f1b3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NTAxMTZ8MHwxfHNlYXJjaHwzfHxyaHl0aG1pYyUyMGd5bW5hc3RpY3MlMjBob29wJTIwZ2lybHxlbnwwfHx8fDE3NjYxNjI3MTB8MA&ixlib=rb-4.1.0&q=80&w=1080"
-  ];
+  const images = [moment1, moment2, moment3, moment4, moment5, moment6];
 
   return (
     <section id="gallery" className="py-20 bg-cream/50">
