a69a94f3fc07c579b9204037a01464243356a150
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-20 21:11:01 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-lavender text-white/60 py-8 text-center">
       <div className="container mx-auto px-6">
         <p className="font-serif">
-          &copy; {new Date().getFullYear()} Maria Magdalena Zelenina. All rights reserved.
+          &copy; {new Date().getFullYear()} Maria Magdalena Zelenina. All rights reserved
         </p>
       </div>
     </footer>
