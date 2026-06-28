6f71ab651cd707bd9ec1d8b7b9908e2339176c34
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2026-06-28 14:39:46 +0300

    Update image imports in PhotoGallery.tsx
    Replaced image imports in PhotoGallery component.

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -7,9 +7,9 @@ import img3 from '../assets/Maria3.jpg';
 import img4 from '../assets/5.jpg';
 import img5 from '../assets/6.jpg';
 import img6 from '../assets/7.jpg';
-import img7 from '../assets/2.jpg';
-import img8 from '../assets/3.jpg';
-import img9 from '../assets/4.jpg';
+import img7 from '../assets/1.jpg';
+import img8 from '../assets/2.jpg';
+import img9 from '../assets/3.jpg';
 
 // 👉 порядок ровно такой, как ты хочешь
 const images: string[] = [
