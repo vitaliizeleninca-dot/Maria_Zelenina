8d4229516fef774ad4adbe47fd8b6444f2ba46eb
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2026-06-28 14:49:09 +0300

    Update PhotoGallery.tsx

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import img1 from '../assets/Maria1.jpg';
 import img2 from '../assets/Maria2.jpg';
 import img3 from '../assets/Maria3.jpg';
-import img4 from '../assets/5.jpg';
+import img4 from '../assets/4.jpg';
 import img5 from '../assets/6.jpg';
 import img6 from '../assets/7.jpg';
 import img7 from '../assets/1.jpg';
